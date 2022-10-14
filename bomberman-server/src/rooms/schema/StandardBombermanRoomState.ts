a00763a6c558650cb2beed2de099832f68da662f
Alex Lau <alex@tecky.io>
2022-10-14 22:41:52 +0800

    Added bomberman game state

--- bomberman-server/src/rooms/schema/StandardBombermanRoomState.ts
+++ bomberman-server/src/rooms/schema/StandardBombermanRoomState.ts
@@ -1,7 +1,17 @@
-import { Schema, Context, type } from "@colyseus/schema";
+import { Schema, ArraySchema, type } from "@colyseus/schema";
+
+class Player extends Schema {
+  @type("number") x: number;
+  @type("number") y: number;
+}
+
+class Block extends Schema {
+  @type("number") type: 'empty' | 'stone' | 'wall';
+  @type("boolean") item: 'bomb' | 'powerup-bomb' | null;
+}
 
 export class StandardBombermanRoomState extends Schema {
-
-  @type("string") mySynchronizedProperty: string = "Hello world";
-
+  @type([ Block ]) blocks = new ArraySchema<Block>();
+  @type([ Player ]) players = new ArraySchema<Player>();
+  @type("number") timeRemaining: number;
 }
